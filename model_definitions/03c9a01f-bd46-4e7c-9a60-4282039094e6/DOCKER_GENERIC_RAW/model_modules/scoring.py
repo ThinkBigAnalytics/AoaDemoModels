d7ebc875f8f1aa04cc7dd95cbc536451a46069fa
Will Fleury <willfleury@gmail.com>
2019-03-06 20:48:10 +0100

    improve xgboost example and add notebook

--- model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/DOCKER_GENERIC_RAW/model_modules/scoring.py
+++ model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/DOCKER_GENERIC_RAW/model_modules/scoring.py
@@ -1,5 +1,4 @@
 from numpy import loadtxt
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
 import json
@@ -8,11 +7,14 @@ import json
 class ModelScorer(object):
     def __init__(self, config=None):
         self.model = pickle.load(open("models/model.pkl", 'rb'))
+        self.scaler = pickle.load(open("models/scaler.pkl", 'rb'))
 
     def predict(self, data):
+        data = self.scaler.transform([data])
         return self.model.predict([data])
 
     def evaluate(self, x, y):
+        x = self.scaler.transform(x)
         y_pred = self.model.predict(x)
 
         predictions = [round(value) for value in y_pred]
@@ -23,17 +25,11 @@ class ModelScorer(object):
 
 def evaluate(data_conf, model_conf, **kwargs):
 
-    # load data
     dataset = loadtxt(data_conf['data_path'], delimiter=",")
 
     # split data into X and y
-    X = dataset[:,0:8]
-    Y = dataset[:,8]
-
-    # split data into train and test sets
-    seed = 7
-    test_size = 0.33
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+    X_test = dataset[:,0:8]
+    y_test = dataset[:,8]
 
     scorer = ModelScorer(model_conf)
     scores = scorer.evaluate(X_test, y_test)
