02489c1f0a473b148def9084b1edfb9993a67a6d
Will Fleury <willfleury@gmail.com>
2019-03-06 20:48:10 +0100

    improve xgboost example and add notebook

--- model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/DOCKER_GENERIC_RAW/model_modules/training.py
+++ model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/DOCKER_GENERIC_RAW/model_modules/training.py
@@ -1,32 +1,32 @@
 from numpy import loadtxt
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler as Scaler
 import pickle
 
 
 def train(data_conf, model_conf, **kwargs):
+    hyperparams = model_conf["hyperparameters"]
 
-    # load data
     dataset = loadtxt(data_conf['data_path'], delimiter=",")
 
     # split data into X and y
-    X = dataset[:,0:8]
-    Y = dataset[:,8]
-
-    # split data into train and test sets
-    seed = 7
-    test_size = 0.33
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+    X_train = dataset[:, 0:8]
+    y_train = dataset[:, 8]
+    
+    scaler = Scaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
 
     print("Starting training...")
 
     # fit model no training data
-    model = XGBClassifier()
+    model = XGBClassifier(eta=hyperparams["eta"], max_depth=hyperparams["max_depth"])
     model.fit(X_train, y_train)
 
     print("Finished training")
 
-    # save model to pickle file
+    # save scaler & model to pickle file
+    pickle.dump(scaler, open("models/scaler.pkl", "wb"))
     pickle.dump(model, open("models/model.pkl", "wb"))
 
     print("Saved trained model")
