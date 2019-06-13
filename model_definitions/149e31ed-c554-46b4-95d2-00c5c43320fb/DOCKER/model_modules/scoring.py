b8781835053a0d2ba14444f6d1c2c91189af6793
Will Fleury <willfleury@gmail.com>
2019-06-13 17:00:37 +0200

    added pyspark notebook

--- model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/DOCKER/model_modules/scoring.py
+++ model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/DOCKER/model_modules/scoring.py
@@ -1,10 +1,17 @@
+import json
+
+from pyspark.ml.classification import LogisticRegressionModel
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 
 def evaluate(spark, data_conf, model_conf, **kwargs):
-
-    # load data & engineer
-
-    # evaluate
-
-    # dump results as json file evaluation.json to models/ folder
-    print("Evaluation complete...")
+    lr_model = LogisticRegressionModel.load(spark.sparkContext._conf.get("spark.ammf.model.path", "file:///tmp/lr"))
+    test = spark.read.format("libsvm").load(data_conf["data_path"])
+    predictions = lr_model.transform(test)
+    
+    evaluator = BinaryClassificationEvaluator()
+    roc = evaluator.evaluate(predictions)
+    print('Test Area Under ROC: {}'.format(roc))
+    
+    with open("models/evaluation.json", "w+") as f:
+        json.dump({'roc': roc}, f)
