f1385a40c9fca833e265ef414c404ec576c23497
root <root@teradata.com>
2019-06-28 15:36:11 +0000

    my demo changes

--- model_definitions/920ebf0e-1f0e-442a-94d1-214f63b8b820/DOCKER/model_modules/training.py
+++ model_definitions/920ebf0e-1f0e-442a-94d1-214f63b8b820/DOCKER/model_modules/training.py
@@ -4,6 +4,7 @@ from teradataml import create_context
 from teradataml.dataframe.dataframe import DataFrame
 from teradataml.analytics.mle import XGBoost
 from teradataml.options.display import display
+from teradataml.context.context import get_connection
 
 display.print_sqlmr_query = True
 
@@ -31,13 +32,19 @@ def train(data_conf, model_conf, **kwargs):
                   min_node_size=1,
                   max_depth=hyperparams["max_depth"])
 
+    # force evaluation as its lazy otherwise
+    xgb.model_table.head(1)
+    
     print("Finished training")
 
-    # export model artefacts
-    xgb.model_table.to_sql(table_name=data_conf["model_table"], if_exists="replace")
+    # export model artefacts. work around bug in data frame code
+    # xgb.model_table.to_sql(table_name=data_conf["model_table"], if_exists="replace")
+    get_connection().execute("CREATE TABLE {} AS (SELECT * FROM {}) WITH DATA"
+                             .format(data_conf['model_table'], xgb.model_table._table_name))
+    
 
-    #model = xgb.model_table.to_pandas()
-    #model.to_hdf("models/model.h5", key="model", mode="w")
+    # model = xgb.model_table.to_pandas()
+    # model.to_hdf("models/model.h5", key="model", mode="w")
 
     print("Saved trained model")
     
