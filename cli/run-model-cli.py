dd9cde42c734dfa0c40ce675aa4f7f361edb19d6
Will Fleury <willfleury@gmail.com>
2019-10-28 16:04:45 +0100

    improve the cli tool

--- cli/run-model-cli.py
+++ cli/run-model-cli.py
@@ -21,15 +21,33 @@ def main():
     from argparse import ArgumentParser
 
     parser = ArgumentParser(description='Run model training or scoring locally')
-    parser.add_argument('model_id', type=str, help='Which model_id to use')
-    parser.add_argument('mode', type=str, help='The model (train or evaluate)')
-    parser.add_argument('-d', '--data', type=str, help='Json file containing data configuration')
+    parser.add_argument('-id', '--model_id', type=str, help='Which model_id to use (prompted to select if not provided)')
+    parser.add_argument('-m', '--mode', type=str, help='The model (train or evaluate) (prompted to select if not provided)')
+    parser.add_argument('-d', '--data', type=str, help='Json file containing data configuration (prompted to select if not provided)')
 
     args = parser.parse_args()
 
     base_path = os.path.dirname(os.path.realpath(__file__)) + "/../"
 
-    model_dir = base_path + "./model_definitions/" + args.model_id
+    if not args.model_id:
+        model_id = get_model_id(base_path + "./model_definitions/")
+    else:
+        model_id = args.model_id
+
+    if not args.mode:
+        mode = input("Select mode (train, evaluate): ")
+    else:
+        mode = args.mode
+
+    model_dir = base_path + "./model_definitions/" + model_id
+
+    if not args.data:
+        data = get_dataset_metadata(model_dir)
+    else:
+        data = args.data
+
+    with open(data, 'r') as f:
+        data_conf = json.load(f)
 
     with open(model_dir + "/model.json", 'r') as f:
         model_definition = json.load(f)
@@ -37,15 +55,9 @@ def main():
     with open(model_dir + "/config.json", 'r') as f:
         model_conf = json.load(f)
 
-    if not args.data:
-        raise Exception("Dataset metadata must be specified.")
-
-    with open(args.data, 'r') as f:
-        data_conf = json.load(f)
-
-    if os.path.exists('models'):
+    if os.path.exists("models"):
         logging.info("Cleaning directory {} to store test model artefacts".format(os.getcwd() + "/models"))
-        shutil.rmtree("path_to_dir")
+        shutil.rmtree("models")
 
     else:
         logging.info("Creating directory {} to store test model artefacts".format(os.getcwd() + "/models"))
@@ -55,23 +67,65 @@ def main():
         sys.path.append(model_dir)
         import model_modules
 
-        if args.mode == "train":
+        if mode == "train":
             model_modules.training.train(data_conf, model_conf)
-        elif args.mode == "evaluate":
+        elif mode == "evaluate":
             model_modules.scoring.evaluate(data_conf, model_conf)
         else:
-            raise Exception("Unsupported mode used: " + args.mode)
+            raise Exception("Unsupported mode used: " + mode)
 
     elif model_definition["language"] == "sql":
-        if args.mode == "train":
+        if mode == "train":
             train_sql(model_dir, data_conf, model_conf)
-        elif args.mode == "evaluate":
+        elif mode == "evaluate":
             evaluate_sql(model_dir, data_conf, model_conf)
         else:
-            raise Exception("Unsupported mode used: " + args.mode)
+            raise Exception("Unsupported mode used: " + mode)
 
     else:
         raise Exception("Unsupported cli language: {}".format(model_definition["language"]))
+
+
+def get_model_id(models_dir):
+    catalog = {}
+    index = 0
+
+    for model in os.listdir(models_dir):
+        if os.path.exists(models_dir + model + "/model.json"):
+            with open(models_dir + model + "/model.json", 'r') as f:
+                model_definition = json.load(f)
+
+                catalog[index] = model_definition
+
+                index += 1
+
+    for key in catalog:
+        print("({}) {}".format(key, catalog[key]["name"]))
+
+    index = input("Select Model: ")
+
+    return catalog[int(index)]["id"]
+
+
+def get_dataset_metadata(model_dir):
+    catalog = {}
+    index = 0
+
+    if not os.path.exists(model_dir + "/.cli/datasets"):
+        raise Exception("No .cli/datasets exist in model folder. You must specify dataset metadata via -d argument.")
+
+    base_path = model_dir + "/.cli/datasets/"
+    for filename in os.listdir(base_path):
+        with open(base_path + filename, 'r') as f:
+            catalog[index] = filename
+            index += 1
+
+    for key in catalog:
+        print("({}) {}".format(key, catalog[key]))
+
+    index = input("Select Dataset Metadata: ")
+
+    return base_path + catalog[int(index)]
 
 
 def evaluate_sql(model_dir, data_conf, model_conf):
@@ -143,6 +197,7 @@ def execute_sql_script(conn, filename, jinja_ctx):
                     logging.warning("Ignoring DROP statement exception")
                 else:
                     raise e
+
 
 if __name__ == "__main__":
     main()
