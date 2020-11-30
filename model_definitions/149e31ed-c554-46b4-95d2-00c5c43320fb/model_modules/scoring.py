18ace5a8f8b959477b235ad0be1ba20579acb93f
Will Fleury <willfleury@users.noreply.github.com>
2020-11-30 17:05:04 +0100

    issue #64 update pyspark model to use xgboost (#68)
    * issue #64 update pyspark model to use xgboost

--- model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/model_modules/scoring.py
+++ model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/model_modules/scoring.py
@@ -1,12 +1,14 @@
-import json
-import logging
-
-from pyspark.ml.classification import LogisticRegressionModel
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import monotonically_increasing_id
-from .util import read_dataset_from_url
+from sklearn import metrics
+
+import logging
+import joblib
+import urllib.request
+import pandas as pd
+import json
+import os
+
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
 
@@ -16,33 +18,109 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 
+def read_dataframe(url):
+    # in a real world scenario, you would read from S3, HDFS, Teradata,
+    # etc but for demo reading from url. we could read via pandas.read_csv but just to show pyspark ...
+    urllib.request.urlretrieve(url, "/tmp/data.csv")
+    column_names = ["NumTimesPrg", "PlGlcConc", "BloodP", "SkinThick", "TwoHourSerIns", "BMI", "DiPedFunc", "Age", "HasDiabetes"]
+
+    return spark.read.format("csv")\
+        .option("inferSchema", "true")\
+        .load("/tmp/data.csv")\
+        .toDF(*column_names)
+
+
 def score(data_conf, model_conf, **kwargs):
-    lr_model = LogisticRegressionModel.load(spark.conf.get("spark.aoa.modelPath"))
 
-    test = read_dataset_from_url(spark, data_conf["url"])
-    # we need to ensure that test has an id for comparison (almost all datasets will already have a primary key!!)
-    test = test.select("*").withColumn("id", monotonically_increasing_id())
+    # load the model
+    model = joblib.load('artifacts/input/model.joblib')
 
-    predictions = lr_model.transform(test).select("id", "rawPrediction", "prediction", "probability")
+    test_df = read_dataframe(data_conf["url"])
 
-    predictions.write.mode("overwrite").save(data_conf["predictions"])
+    # do feature eng in spark / joins whatever reason you're using pyspark...
+    # split into test and train
+    test_df = test_df.randomSplit([0.7, 0.3], 42)[1].toPandas()
+
+    X_test = test_df[model.feature_names]
+
+    print("Scoring")
+    y_pred = model.predict(X_test)
+
+    y_pred = pd.DataFrame(y_pred, columns=["pred"])
+
+    # wrap as pyspark df
+    predictions = spark.createDataFrame(y_pred)
+
+    # in a real world you would write the results back to HDFS, Teradata, S3 etc.
+    predictions.write.mode("overwrite").save("/tmp/predictions")
+
     logging.info("Finished scoring")
+
+    return X_test, y_pred, model
+
+
+def save_plot(title):
+    import matplotlib.pyplot as plt
+
+    plt.title(title)
+    fig = plt.gcf()
+    filename = title.replace(" ", "_").lower()
+    fig.savefig('artifacts/output/{}'.format(filename), dpi=500)
+    plt.clf()
 
 
 def evaluate(data_conf, model_conf, **kwargs):
 
-    score(data_conf, model_conf, **kwargs)
+    X_test, y_pred, model = score(data_conf, model_conf, **kwargs)
 
-    test = read_dataset_from_url(spark, data_conf["url"])
+    # again, in a real world you would read from S3, HDFS, Teradata and join the y_pred index to read the expected
+    # values for the dataset you scored
+    y_test = read_dataframe(data_conf["url"]).select("HasDiabetes")
+    y_test = y_test.randomSplit([0.7, 0.3], 42)[1].toPandas()
 
-    expected = test.select("*").withColumn("id", monotonically_increasing_id())
-    actual = spark.read.load(data_conf["predictions"])
+    evaluation = {
+        'Accuracy': '{:.2f}'.format(metrics.accuracy_score(y_test, y_pred)),
+        'Recall': '{:.2f}'.format(metrics.recall_score(y_test, y_pred)),
+        'Precision': '{:.2f}'.format(metrics.precision_score(y_test, y_pred)),
+        'f1-score': '{:.2f}'.format(metrics.f1_score(y_test, y_pred))
+    }
 
-    evaluator = BinaryClassificationEvaluator()
-    roc = evaluator.evaluate(expected.join(actual, expected.id == actual.id))
-    logging.info('Test Area Under ROC: {}'.format(roc))
+    with open("artifacts/output/metrics.json", "w+") as f:
+        json.dump(evaluation, f)
 
-    with open("artifacts/output/evaluation.json", "w+") as f:
-        json.dump({'roc': roc}, f)
+    metrics.plot_confusion_matrix(model, X_test, y_test)
+    save_plot('Confusion Matrix')
 
-    logging.info("Finished Evaluation")
+    metrics.plot_roc_curve(model, X_test, y_test)
+    save_plot('ROC Curve')
+
+    # xgboost has its own feature importance plot support but lets use shap as explainability example
+    import shap
+
+    shap_explainer = shap.TreeExplainer(model['xgb'])
+    shap_values = shap_explainer.shap_values(X_test)
+
+    shap.summary_plot(shap_values, X_test, feature_names=model.feature_names,
+                      show=False, plot_size=(12,8), plot_type='bar')
+    save_plot('SHAP Feature Importance')
+
+
+# Add code required for RESTful API
+class ModelScorer(object):
+
+    def __init__(self, config=None):
+        self.model = joblib.load('artifacts/input/model.joblib')
+
+        from prometheus_client import Counter
+        self.pred_class_counter = Counter('model_prediction_classes',
+                                          'Model Prediction Classes', ['model', 'version', 'clazz'])
+
+    def predict(self, data):
+        pred = self.model.predict([data])
+
+        # record the predicted class so we can check model drift (via class distributions)
+        self.pred_class_counter.labels(model=os.environ["MODEL_NAME"],
+                                       version=os.environ.get("MODEL_VERSION", "1.0"),
+                                       clazz=str(int(pred))).inc()
+
+        return pred
