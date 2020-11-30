97139e35a9d8fdb492e35c3902e06e64111fa87d
Will Fleury <willfleury@users.noreply.github.com>
2020-11-30 17:05:04 +0100

    issue #64 update pyspark model to use xgboost (#68)
    * issue #64 update pyspark model to use xgboost

--- model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/model_modules/training.py
+++ model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/model_modules/training.py
@@ -1,9 +1,13 @@
-import logging
-
-from pyspark.ml.classification import LogisticRegression
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from .util import read_dataset_from_url
+from xgboost import XGBClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from nyoka import xgboost_to_pmml
+
+import logging
+import joblib
+import urllib.request
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
 
@@ -16,24 +20,43 @@ spark = SparkSession.builder \
 def train(data_conf, model_conf, **kwargs):
     hyperparams = model_conf["hyperParameters"]
 
-    train = read_dataset_from_url(spark, data_conf["url"])
+    feature_names = ["NumTimesPrg", "PlGlcConc", "BloodP", "SkinThick", "TwoHourSerIns", "BMI", "DiPedFunc", "Age"]
+    target_name = "HasDiabetes"
 
-    lr = LogisticRegression(maxIter=hyperparams["maxIter"],
-                            regParam=hyperparams["regParam"],
-                            elasticNetParam=hyperparams["elasticNetParam"])
+    # in a real world scenario, you would read from S3, HDFS, Teradata,
+    # etc but for demo reading from url. we could read via pandas.read_csv but just to show pyspark ...
+    urllib.request.urlretrieve(data_conf["url"], "/tmp/data.csv")
+    all_columns = feature_names + [target_name]
+    train_df = spark.read.format("csv")\
+        .option("inferSchema", "true")\
+        .load("/tmp/data.csv")\
+        .toDF(*all_columns)
 
-    logging.info("Starting training...")
+    # do feature eng in spark / joins whatever reason you're using pyspark...
+    # split into test and train
+    train_df = train_df.randomSplit([0.7, 0.3], 42)[0].toPandas()
 
-    lr_model = lr.fit(train)
+    # split data into X and y
+    X_train = train_df.drop(target_name, 1)
+    y_train = train_df[target_name]
 
-    # Print the coefficients and intercept for logistic regression
-    logging.debug("Coefficients: {}".format(str(lr_model.coefficients)))
-    logging.debug("Intercept: {}".format(str(lr_model.intercept)))
+    print("Starting training...")
 
-    logging.info("Finished training")
+    # fit model to training data
+    model = Pipeline([('scaler', MinMaxScaler()),
+                      ('xgb', XGBClassifier(eta=hyperparams["eta"],
+                                            max_depth=hyperparams["max_depth"]))])
+    # xgboost saves feature names but lets store on pipeline for easy access
+    model.feature_names = feature_names
 
-    # export model directly to s3 model artifacts folder
-    lr_model.write().overwrite().save(spark.conf.get("spark.aoa.modelPath"))
-    logging.info("Saved trained model")
+    model.fit(X_train, y_train)
 
-    return lr_model
+    print("Finished training")
+
+    # export model artefacts
+    joblib.dump(model, "artifacts/output/model.joblib")
+
+    # we can also save as pmml so it can be used for In-Vantage scoring etc.
+    xgboost_to_pmml(pipeline=model, col_names=feature_names[0:8], target_name=target_name, pmml_f_name="artifacts/output/model.pmml")
+
+    print("Saved trained model")
