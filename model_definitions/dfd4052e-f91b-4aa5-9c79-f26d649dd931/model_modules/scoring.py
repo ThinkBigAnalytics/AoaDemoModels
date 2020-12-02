d9527061cb70946feed283e3708e43a4743e0ce6
Will Fleury <willfleury@gmail.com>
2020-12-02 16:16:31 +0100

    issue #69 working STOs..

--- model_definitions/dfd4052e-f91b-4aa5-9c79-f26d649dd931/model_modules/scoring.py
+++ model_definitions/dfd4052e-f91b-4aa5-9c79-f26d649dd931/model_modules/scoring.py
@@ -2,8 +2,7 @@ from teradataml import create_context
 from tdextensions.distributed import DistDataFrame, DistMode
 from teradataml.dataframe.dataframe import DataFrame
 
-import base64
-import dill
+import random
 import os
 import numpy as np
 import json
@@ -20,6 +19,47 @@ def score(data_conf, model_conf, **kwargs):
     None:No return
 
     """
+
+
+def eval_partition(partition):
+
+    # do evaluation
+
+    partition_id = partition.species.iloc[0]
+
+    # whatever evaluation metadata you want to record goes here. Like the evaluation metrics, etc
+    partition_metadata = json.dumps({
+        "num_rows": partition.shape[0],
+        "metrics": {
+            "accuracy": random.uniform(60.0, 95.0),
+            "precision": random.uniform(60.0, 95.0)
+        }
+    })
+
+    return np.array([[partition_id, partition.shape[0], partition_metadata]])
+
+
+def evaluate(data_conf, model_conf, **kwargs):
+    model_version = kwargs["model_version"]
+
+    create_context(host="host.docker.internal", username=os.environ['TD_USERNAME'], password=os.environ['TD_PASSWORD'])
+
+    df = DataFrame.from_query("""
+    SELECT i.*, m.model_artefact FROM iris_train i 
+        LEFT JOIN aoa_sto_models m ON i.species = m.partition_id 
+        WHERE model_version='{}'
+    """.format(model_version))
+
+    df = DistDataFrame(df._table_name, dist_mode=DistMode.STO, sto_id="my_model")
+    eval_df = df.map_partition(lambda partition: eval_partition(partition),
+                               partition_by="species",
+                               returns=[["partition_id", "VARCHAR(255)"],
+                                        ["num_rows", "BIGINT"],
+                                        ["partition_metadata", "CLOB"]])
+
+    eval_df.to_sql("aoa_sto_models", if_exists="append")
+
+    save_metadata(eval_df)
 
 
 def save_metadata(df):
@@ -39,41 +79,3 @@ def save_metadata(df):
         json.dump(data_metadata, f, indent=2)
 
     print("Finished saving artefacts")
-
-
-def evaluate(data_conf, model_conf, **kwargs):
-    model_version = kwargs["model_version"]
-
-    create_context(host="host.docker.internal", username=os.environ['TD_USERNAME'], password=os.environ['TD_PASSWORD'])
-
-    df = DataFrame.from_query("""
-    SELECT i.*, m.model_artefact FROM iris_train i 
-        LEFT JOIN aoa_sto_models m ON i.species = m.partition_id 
-        WHERE model_version='{}'
-    """.format(model_version))
-
-    def eval_partition(partition):
-
-        # do evaluation
-
-        partition_id = partition.species.iloc[0]
-
-        # whatever evaluation metadata you want to record goes here. Like the evaluation metrics, etc
-        partition_metadata = json.dumps({
-            "num_rows": partition.shape[0],
-            "metrics": {
-                "accuracy": 0.9,
-                "precision": 0.87
-            }
-        })
-
-        return np.array([[partition_id, partition.shape[0], partition_metadata]])
-
-    df = DistDataFrame(df._table_name, dist_mode=DistMode.STO, sto_id="my_model")
-    eval_df = df.map_partition(lambda partition: eval_partition(partition),
-                               partition_by="species",
-                               returns=[["partition_id", "VARCHAR(255)"],
-                                        ["num_rows", "BIGINT"],
-                                        ["partition_metadata", "CLOB"]])
-
-    save_metadata(eval_df)
--- model_definitions/dfd4052e-f91b-4aa5-9c79-f26d649dd931/model_modules/training.py
+++ model_definitions/dfd4052e-f91b-4aa5-9c79-f26d649dd931/model_modules/training.py
@@ -9,6 +9,57 @@ import base64
 import dill
 
 
+def train_partition(partition, model_version, hyperparams):
+    X = partition[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
+    y = partition[['species']]
+
+    clf = RandomForestClassifier()
+    clf.fit(X, y.values.ravel())
+
+    partition_id = partition.species.iloc[0]
+    artefact = base64.b64encode(dill.dumps(clf))
+
+    # partition metadata allows you to record whatever partition level information you want from data statistics,
+    # to hyper parameters used to model explainability
+    partition_metadata = json.dumps({
+        "num_rows": partition.shape[0],
+        # "data_statistics": json.loads(partition.describe().to_json())
+        # "explainability": shap.....
+        "hyper_parameters": hyperparams
+    })
+
+    return np.array([[partition_id, model_version, partition.shape[0], partition_metadata, artefact]])
+
+
+def train(data_conf, model_conf, **kwargs):
+    model_version = kwargs["model_version"]
+    hyperparams = model_conf["hyperParameters"]
+
+    create_context(host="host.docker.internal", username=os.environ['TD_USERNAME'], password=os.environ['TD_PASSWORD'])
+
+    # hack until cli cleans this table up automatically or we allow to override on run
+    from teradataml.context.context import get_connection
+    get_connection().execute("DELETE FROM aoa_sto_models WHERE model_version='{}'".format(model_version))
+
+    print("Starting training...")
+
+    df = DistDataFrame("iris_train", dist_mode=DistMode.STO, sto_id="my_model")
+    model_df = df.map_partition(lambda partition: train_partition(partition, model_version, hyperparams),
+                                partition_by="species",
+                                returns=[["partition_id", "VARCHAR(255)"],
+                                         ["model_version", "VARCHAR(255)"],
+                                         ["num_rows", "BIGINT"],
+                                         ["partition_metadata", "CLOB"],
+                                         ["model_artefact", "CLOB"]])
+
+    # append to models table
+    model_df.to_sql("aoa_sto_models", if_exists="append")
+
+    print("Finished training")
+
+    save_metadata(model_df)
+
+
 def save_metadata(model_df):
     # convert stats to dict and save to partitions.json
     metadata_df = model_df.select(["partition_id", "partition_metadata", "num_rows"]).to_pandas()
@@ -26,53 +77,3 @@ def save_metadata(model_df):
         json.dump(data_metadata, f, indent=2)
 
     print("Finished saving artefacts")
-
-
-def train(data_conf, model_conf, **kwargs):
-    model_version = kwargs["model_version"]
-    hyperparams = model_conf["hyperParameters"]
-
-    create_context(host="host.docker.internal", username=os.environ['TD_USERNAME'], password=os.environ['TD_PASSWORD'])
-
-    # hack until cli cleans this table up automatically or we allow to override on run
-    from teradataml.context.context import get_connection
-    get_connection().execute("DELETE FROM aoa_sto_models WHERE model_version='{}'".format(model_version))
-
-    def train_partition(partition, model_version):
-        X = partition[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-        y = partition[['species']]
-
-        clf = RandomForestClassifier()
-        clf.fit(X, y.values.ravel())
-
-        partition_id = partition.species.iloc[0]
-        artefact = base64.b64encode(dill.dumps(clf))
-
-        # partition metadata allows you to record whatever partition level information you want from data statistics,
-        # to hyper parameters used to model explainability
-        partition_metadata = json.dumps({
-            "num_rows": partition.shape[0],
-            # "data_statistics": json.loads(partition.describe().to_json())
-            # "explainability": shap.....
-            "hyper_parameters": hyperparams
-        })
-
-        return np.array([[partition_id, model_version, partition.shape[0], partition_metadata, artefact]])
-
-    print("Starting training...")
-
-    df = DistDataFrame("iris_train", dist_mode=DistMode.STO, sto_id="my_model")
-    model_df = df.map_partition(lambda partition: train_partition(partition, model_version),
-                                partition_by="species",
-                                returns=[["partition_id", "VARCHAR(255)"],
-                                         ["model_version", "VARCHAR(255)"],
-                                         ["num_rows", "BIGINT"],
-                                         ["partition_metadata", "CLOB"],
-                                         ["model_artefact", "CLOB"]])
-
-    # append to models table
-    model_df.to_sql("aoa_sto_models", if_exists="append")
-
-    print("Finished training")
-
-    save_metadata(model_df)
