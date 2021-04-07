2b06f897a99679ca3ea56d9586eac27c0694cc33
Will Fleury <willfleury@users.noreply.github.com>
2021-04-07 12:29:51 +0200

    backout R changes

--- model_definitions/bf6a52b2-b595-4358-ac4f-24fb41a85c45/model_modules/requirements.R
+++ model_definitions/bf6a52b2-b595-4358-ac4f-24fb41a85c45/model_modules/requirements.R
@@ -2,7 +2,6 @@ message('Installing packages')
 if(!require('gbm')){install.packages('gbm')}
 if(!require('devtools')){install.packages('devtools')}
 if(!require('caret')){install.packages('caret')}
-if(!require('R.utils')){install.packages('R.utils')}
 
 #library("devtools")
 #install_git("git://github.com/jpmml/r2pmml.git")
