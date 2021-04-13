6235bfc16492862b68ac11e91a30fee74a210741
Will Fleury <willfleury@gmail.com>
2021-04-13 22:14:36 +0200

    Revert "issue #83 add dataset statistics (#84)"
    This reverts commit 50ce9802286750c9af73da70962b688018ab2150.

--- model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/model_modules/evaluation.py
+++ model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/model_modules/evaluation.py
@@ -1,15 +1,20 @@
 from sklearn import metrics
 from teradataml import create_context
 from teradataml.dataframe.dataframe import DataFrame
-from aoa.stats import stats
-from aoa.util.artefacts import save_plot
 
 import os
 import joblib
 import json
-import numpy as np
-import pandas as pd
 
+
+def save_plot(title):
+    import matplotlib.pyplot as plt
+
+    plt.title(title)
+    fig = plt.gcf()
+    filename = title.replace(" ", "_").lower()
+    fig.savefig('artifacts/output/{}'.format(filename), dpi=500)
+    plt.clf()
 
 
 def evaluate(data_conf, model_conf, **kwargs):
@@ -23,13 +28,13 @@ def evaluate(data_conf, model_conf, **kwargs):
     # As this is for demo purposes, we simulate the test dataset changing between executions
     # by introducing a random sample. Note that the sampling is performed in Teradata!
     test_df = DataFrame(data_conf["table"]).sample(frac=0.8)
-    test_pdf = test_df.to_pandas()
+    test_df = test_df.to_pandas()
 
-    X_test = test_pdf[model.feature_names]
-    y_test = test_pdf[model.target_name]
+    X_test = test_df[model.feature_names]
+    y_test = test_df[model.target_name]
 
     print("Scoring")
-    y_pred = model.predict(test_pdf[model.feature_names])
+    y_pred = model.predict(test_df[model.feature_names])
 
     evaluation = {
         'Accuracy': '{:.2f}'.format(metrics.accuracy_score(y_test, y_pred)),
@@ -54,16 +59,5 @@ def evaluate(data_conf, model_conf, **kwargs):
     shap_values = shap_explainer.shap_values(X_test)
 
     shap.summary_plot(shap_values, X_test, feature_names=model.feature_names,
-                      show=False, plot_size=(12, 8), plot_type='bar')
+                      show=False, plot_size=(12,8), plot_type='bar')
     save_plot('SHAP Feature Importance')
-
-    feature_importance = pd.DataFrame(list(zip(model.feature_names, np.abs(shap_values).mean(0))),
-                                      columns=['col_name', 'feature_importance_vals'])
-    feature_importance = feature_importance.set_index("col_name").T.to_dict(orient='records')[0]
-
-    stats.record_stats(test_df,
-                       features=model.feature_names,
-                       predictors=["HasDiabetes"],
-                       categorical=["HasDiabetes"],
-                       importance=feature_importance,
-                       category_labels={"HasDiabetes": {0: "false", 1: "true"}})
--- model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/model_modules/training.py
+++ model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/model_modules/training.py
@@ -4,8 +4,6 @@ from sklearn.pipeline import Pipeline
 from nyoka import xgboost_to_pmml
 from teradataml import create_context
 from teradataml.dataframe.dataframe import DataFrame
-from aoa.stats import stats
-from aoa.util.artefacts import save_plot
 
 import joblib
 import os
@@ -24,18 +22,18 @@ def train(data_conf, model_conf, **kwargs):
     # read training dataset from Teradata and convert to pandas
     train_df = DataFrame(data_conf["table"])
     train_df = train_df.select([feature_names + [target_name]])
-    train_pdf = train_df.to_pandas()
+    train_df = train_df.to_pandas()
 
     # split data into X and y
-    X_train = train_pdf.drop(target_name, 1)
-    y_train = train_pdf[target_name]
+    X_train = train_df.drop(target_name, 1)
+    y_train = train_df[target_name]
 
     print("Starting training...")
 
     # fit model to training data
     model = Pipeline([('scaler', MinMaxScaler()),
-                      ('xgb', XGBClassifier(eta=hyperparams["eta"],
-                                            max_depth=hyperparams["max_depth"]))])
+                     ('xgb', XGBClassifier(eta=hyperparams["eta"],
+                                           max_depth=hyperparams["max_depth"]))])
     # xgboost saves feature names but lets store on pipeline for easy access later
     model.feature_names = feature_names
     model.target_name = target_name
@@ -48,20 +46,6 @@ def train(data_conf, model_conf, **kwargs):
     joblib.dump(model, "artifacts/output/model.joblib")
 
     # we can also save as pmml so it can be used for In-Vantage scoring etc.
-    xgboost_to_pmml(pipeline=model, col_names=feature_names, target_name=target_name,
-                    pmml_f_name="artifacts/output/model.pmml")
+    xgboost_to_pmml(pipeline=model, col_names=feature_names, target_name=target_name, pmml_f_name="artifacts/output/model.pmml")
 
     print("Saved trained model")
-
-    from xgboost import plot_importance
-    model["xgb"].get_booster().feature_names = feature_names
-    plot_importance(model["xgb"].get_booster(), max_num_features=10)
-    save_plot("feature_importance.png")
-
-    feature_importance = model["xgb"].get_booster().get_score(importance_type="weight")
-    stats.record_stats(train_df,
-                       features=feature_names,
-                       predictors=["HasDiabetes"],
-                       categorical=["HasDiabetes"],
-                       importance=feature_importance,
-                       category_labels={"HasDiabetes": {0: "false", 1: "true"}})
