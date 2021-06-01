107be9bf01880da6d138883b8b835876a39e8955
Christian Axel Schmidt Dick (Teradata) <christianaxel.schmidt@teradata.com>
2021-06-02 00:10:06 +0200

    Removed requirements from spark model #82
    Fix spark model for VAL stats
    
    Fix spark model evaluation for VAL stats
    
    Fix spark model scoring for new base image
    
    Updated spark model scoring for new base image #87

--- model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/model_modules/training.py
+++ model_definitions/149e31ed-c554-46b4-95d2-00c5c43320fb/model_modules/training.py
@@ -4,11 +4,14 @@ from xgboost import XGBClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
 from nyoka import xgboost_to_pmml
-from .util import read_dataframe
+from teradataml import create_context
+from teradataml.dataframe.dataframe import DataFrame
+from aoa.stats import stats
+from aoa.util.artefacts import save_plot
 
 import logging
 import joblib
-import urllib.request
+import os
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
 
@@ -21,18 +24,25 @@ spark = SparkSession.builder \
 def train(data_conf, model_conf, **kwargs):
     hyperparams = model_conf["hyperParameters"]
 
+    # For demo purposes we read data from Vantage, but in a real environment
+    # it can be anything that pyspark can read (csv, parquet, avro, etc...)
+    create_context(host=os.environ["AOA_CONN_HOST"],
+                   username=os.environ["AOA_CONN_USERNAME"],
+                   password=os.environ["AOA_CONN_PASSWORD"])
+
     feature_names = ["NumTimesPrg", "PlGlcConc", "BloodP", "SkinThick", "TwoHourSerIns", "BMI", "DiPedFunc", "Age"]
     target_name = "HasDiabetes"
 
-    train_df = read_dataframe(spark, data_conf["url"])
+    # read training dataset from Teradata
+    train_df = DataFrame(data_conf["table"])
+    train_df = train_df.select([feature_names + [target_name]])
+    train_pdf = train_df.to_pandas()
 
     # do feature eng in spark / joins whatever reason you're using pyspark...
-    # split into test and train
-    train_df = train_df.randomSplit([0.7, 0.3], 42)[0].toPandas()
 
     # split data into X and y
-    X_train = train_df.drop(target_name, 1)
-    y_train = train_df[target_name]
+    X_train = train_pdf.drop(target_name, 1)
+    y_train = train_pdf[target_name]
 
     print("Starting training...")
 
@@ -42,6 +52,7 @@ def train(data_conf, model_conf, **kwargs):
                                             max_depth=hyperparams["max_depth"]))])
     # xgboost saves feature names but lets store on pipeline for easy access
     model.feature_names = feature_names
+    model.target_name = target_name
 
     model.fit(X_train, y_train)
 
@@ -54,3 +65,16 @@ def train(data_conf, model_conf, **kwargs):
     xgboost_to_pmml(pipeline=model, col_names=feature_names[0:8], target_name=target_name, pmml_f_name="artifacts/output/model.pmml")
 
     print("Saved trained model")
+
+    from xgboost import plot_importance
+    model["xgb"].get_booster().feature_names = feature_names
+    plot_importance(model["xgb"].get_booster(), max_num_features=10)
+    save_plot("feature_importance.png")
+
+    feature_importance = model["xgb"].get_booster().get_score(importance_type="weight")
+    stats.record_stats(train_df,
+                       features=feature_names,
+                       predictors=["HasDiabetes"],
+                       categorical=["HasDiabetes"],
+                       importance=feature_importance,
+                       category_labels={"HasDiabetes": {0: "false", 1: "true"}})
