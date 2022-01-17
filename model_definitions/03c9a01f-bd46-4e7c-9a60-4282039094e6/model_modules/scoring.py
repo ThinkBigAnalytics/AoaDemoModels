ab470259e892063266d9347e1e466eaac5ac3b53
Will Fleury <willfleury@users.noreply.github.com>
2022-01-17 10:04:10 +0100

    #125 fix scoring method for restful (#127)

--- model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/model_modules/scoring.py
+++ model_definitions/03c9a01f-bd46-4e7c-9a60-4282039094e6/model_modules/scoring.py
@@ -37,16 +37,5 @@ class ModelScorer(object):
     def __init__(self, config=None):
         self.model = joblib.load('artifacts/input/model.joblib')
 
-        from prometheus_client import Counter
-        self.pred_class_counter = Counter('model_prediction_classes',
-                                          'Model Prediction Classes', ['model', 'version', 'clazz'])
-
     def predict(self, data):
-        pred = self.model.predict([data])
-
-        # record the predicted class so we can check model drift (via class distributions)
-        self.pred_class_counter.labels(model=os.environ["MODEL_NAME"],
-                                       version=os.environ.get("MODEL_VERSION", "1.0"),
-                                       clazz=str(int(pred))).inc()
-
-        return pred
+        return self.model.predict([data])
