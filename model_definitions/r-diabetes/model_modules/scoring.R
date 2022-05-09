d0354c79084a6c30ae59b9592927c038f92fbdd5
Will Fleury <willfleury@gmail.com>
2022-05-09 14:22:43 +0200

    #129 fixes from testing

--- model_definitions/r-diabetes/model_modules/scoring.R
+++ model_definitions/r-diabetes/model_modules/scoring.R
@@ -3,7 +3,7 @@ library(gbm)
 library(jsonlite)
 library(caret)
 
-LoadPackages <- function() {
+LoadBatchScoringPackages <- function() {
     library("gbm")
     library("DBI")
     library("dplyr")
@@ -17,11 +17,11 @@ score.restful <- function(model, data, ...) {
     score
 }
 
-score.batch <- function(data_conf, model_conf, model_version, ...) {
+score.batch <- function(data_conf, model_conf, model_version, job_id, ...) {
     model <- initialise_model()
     print("Batch scoring model...")
 
-    suppressPackageStartupMessages(LoadPackages())
+    suppressPackageStartupMessages(LoadBatchScoringPackages())
 
     # Connect to Teradata Vantage
     con <- aoa_create_context()
@@ -29,7 +29,8 @@ score.batch <- function(data_conf, model_conf, model_version, ...) {
     table <- tbl(con, sql(data_conf$sql))
 
     # Create dataframe from tibble, selecting the necessary columns and mutating integer64 to integers
-    data <- table %>% mutate(NumTimesPrg = as.integer(NumTimesPrg),
+    data <- table %>% mutate(PatientId = as.integer(PatientId),
+                             NumTimesPrg = as.integer(NumTimesPrg),
                              PlGlcConc = as.integer(PlGlcConc),
                              BloodP = as.integer(BloodP),
                              SkinThick = as.integer(SkinThick),
@@ -41,15 +42,28 @@ score.batch <- function(data_conf, model_conf, model_version, ...) {
     print("Finished batch scoring model...")
 
     # create result dataframe and store in Teradata Vantage
-    score_df <- as.data.frame(unlist(score))
-    colnames(score_df) <- c("Prediction")
-    patientIds <- table %>% select("PatientId") %>% mutate(PatientId = as.integer(PatientId)) %>% as.data.frame()
-    score_df$PatiendId <- patientIds$PatientId
+    pred_df <- as.data.frame(unlist(score))
+    colnames(pred_df) <- c("HasDiabetes")
+    pred_df$PatientId <- data$PatientId
+    pred_df$job_id <- job_id
 
-    # dbplyr::in_schema(data_conf$predictions$database, data_conf$predictions$table)
-    copy_to(con, score_df,
-            name=dbplyr::in_schema(data_conf$predictions$database, "pima_predictions"),
-            overwrite=TRUE)
+    # tdplyr doesn't match column names on append.. and so to match / use same table schema as for byom predict
+    # example (see README.md), we must add empty json_report column and change column order manually (v17.0.0.4)
+    # CREATE MULTISET TABLE pima_patient_predictions
+    # (
+    #     job_id VARCHAR(255), -- comes from airflow on job execution
+    #     PatientId BIGINT,    -- entity key as it is in the source data
+    #     HasDiabetes BIGINT,   -- if model automatically extracts target
+    #     json_report CLOB(1048544000) CHARACTER SET UNICODE  -- output of
+    # )
+    # PRIMARY INDEX ( job_id );
+    pred_df$json_report <- ""
+    pred_df <- pred_df[, c("job_id", "PatientId", "HasDiabetes", "json_report")]
+
+    copy_to(con, pred_df,
+            name=dbplyr::in_schema(data_conf$predictions$database, data_conf$predictions$table),
+            types = c("varchar(255)", "bigint", "bigint", "clob")
+            append=TRUE)
     print("Saved batch predictions...")
 }
 
